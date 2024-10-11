96a6d670e157397e1c41d4f143bd1cc492e5cc1b
DESKTOP-H3VHUFJ\Manon <manon.a.dauguet@orange.fr>
2024-10-11 17:19:36 +0200

    refactor : nouvelle base de configuation sur laquelle partir.

--- src/main/java/com/chatop/api/configuration/SpringSecurityConfig.java
+++ src/main/java/com/chatop/api/configuration/SpringSecurityConfig.java
@@ -1,36 +1,19 @@
 package com.chatop.api.configuration;
 
-import javax.crypto.spec.SecretKeySpec;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SpringSecurityConfig {
-	
-	
-	private String jwtKey = "laclegeneree256….";
 
+	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		return http
-				.csrf(csrf -> csrf.disable()) // Remove for the prod
-				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authorizeHttpRequests(auth -> auth
-						.requestMatchers(new AntPathRequestMatcher("/")).permitAll()
-						.anyRequest().authenticated()).httpBasic(Customizer.withDefaults())
-				.formLogin(form -> form.disable())
-				.build();
+		http.csrf(csrf -> csrf.disable()) // Remove for the prod
+				.authorizeHttpRequests(
+						(auth) -> auth.requestMatchers("/secur/**").authenticated().requestMatchers("/**").permitAll());
+
+		return http.build();
 	}
 }
--- src/main/java/com/chatop/api/controller/UserController.java
+++ src/main/java/com/chatop/api/controller/UserController.java
@@ -3,9 +3,11 @@ package com.chatop.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.chatop.api.service.UserService;
+
 @RestController
 public class UserController {
 
 	@Autowired
-	private UserController userControler;
+	private UserService userService;
 }
